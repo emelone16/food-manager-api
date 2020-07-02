98735940d2f954742a0a3a7c72823d9c6acc4a17
emelone16 <emelone@optonline.net>
2020-07-02 08:45:58 -0400

    configures package.json for publishing eventually

--- src/express/start.ts
+++ src/express/start.ts
@@ -3,8 +3,8 @@ import https from "https";
 import { Backend } from "../types//backend";
 
 const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/erikjmelone.com/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/erikjmelone.com/fullchain.pem")
+  // key: fs.readFileSync("/etc/letsencrypt/live/erikjmelone.com/privkey.pem"),
+  // cert: fs.readFileSync("/etc/letsencrypt/live/erikjmelone.com/fullchain.pem")
 };
 
 const listen = ({ app, port }: Backend) => {
